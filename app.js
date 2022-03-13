d40ece6ed1080263b8ed128c0d11fbeca7f24541
mollikacomputer <mollikacomputer3@gmail.com>
2022-03-13 16:26:05 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const images = [
+    'images/pic-1.jpg',
+    'images/pic-2.jpg',
+    'images/pic-3.jpg',
+    'images/pic-4.jpg',
+    'images/pic-5.jpg'
+];
+let imgIndex = 0;
+const imgElement = document.getElementById('slider-img');
+
+setInterval (()=>{
+    if(imgIndex >= images.length){
+        imgIndex=0;
+    }
+    const imgUrl = images[imgIndex];
+    console.log(imgUrl);
+    imgElement.setAttribute('src', imgUrl);
+    imgIndex++;
+},1000);
